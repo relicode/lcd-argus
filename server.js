12f356e5029a514cf5c11bffbabf702b4a1fd3fd
Anssi Siren <pro@avaruus.mozmail.com>
2024-09-02 15:09:42 +0300

    Add helper response

--- server.js
+++ server.js
@@ -22,7 +22,26 @@ logLines(initialMessage)
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.json({ message })
+  res.json({ message: `
+#!/usr/bin/env node
+
+if (!process.env.DISPLAY_URL) throw new Error('No DISPLAY_URL env variable provided')
+
+const { DISPLAY_URL } = process.env
+const lines = process.argv.slice(2)
+
+const response = await fetch(DISPLAY_URL, {
+  body: JSON.stringify({ lines }, null),
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+if (!response.ok) throw new Error(response.statusText)
+const body = await response.json()
+console.log(body.lines)
+  `, })
 })
 
 app.post('/', async (req, res) => {
